20c065c3c2699ed9f568f46288e4997991018f7f
Stefan Paula <stefan.paula@gmx.at>
2014-04-14 20:04:58 +0200

    Added solution part 9

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,16 +1,16 @@
 var EmployeeView = function(employee) {
- 
-	this.initialize = function() {
+
+    this.initialize = function() {
         this.el = $('<div/>');
     };
-	
-	this.render = function() {
-		this.el.html(EmployeeView.template(employee));
-		return this;
-	};
- 
+
+    this.render = function() {
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+
     this.initialize();
- 
-}
+
+ }
 
 EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
